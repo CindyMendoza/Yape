15de168a1d9504df56e69c70869a65f41c5f499d
cindy <mendozaic@hotmail.com>
2017-06-20 18:33:15 -0500

    hasta checked

--- public/js/components/validNumber.js
+++ public/js/components/validNumber.js
@@ -2,10 +2,21 @@
 
 const validNumber = () =>{
   const divValidNumber = $("<div></div>");
+  const formValidNumber = $("<form></form>");
   const phoneNumber = $("<input type='text' placeholder='Ingrese numero celular'></input>");
   const termsCheckbox = $("<input type='checkbox'>Terminos y condiciones</input>");
-  divValidNumber.append(phoneNumber);
-  divValidNumber.append(termsCheckbox);
-  
+  const btnContinue = $("<button>Continuar</button>");
+  formValidNumber.append(phoneNumber);
+  formValidNumber.append(termsCheckbox);
+  formValidNumber.append(btnContinue);
+  divValidNumber.append(formValidNumber);
+  btnContinue.on("click", function(event) {
+    event.preventDefault();
+    // console.log(phoneNumber.val());
+    // console.log(termsCheckbox.is(':checked') );
+    postRegisterNumber(phoneNumber.val(),termsCheckbox.is(':checked'));
+    
+  })
+
   return divValidNumber;
 }
--- public/js/utils/postRegisterNumber.js
+++ public/js/utils/postRegisterNumber.js
@@ -1,2 +1,10 @@
 'use strict'
- 
+ const postRegisterNumber = (n,t)=>{
+   $.post("api/registerNumber",
+   {"phone":n,"terms":t},
+   (e)=>{
+     console.log(e);
+   },
+   "json"
+ );
+ }
